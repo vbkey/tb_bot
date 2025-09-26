b60aa4840251c95fd3504aa722928b11e8afb2ef
vbkey <vbkey0@gmail.com>
2025-09-26 23:06:30 +0500

    Added profile form

--- main.py
+++ main.py
@@ -4,6 +4,8 @@ from tinydb import TinyDB, Query
 from aiogram import Bot, Dispatcher, Router, F
 from aiogram.types import Message, ReplyKeyboardMarkup, KeyboardButton, InlineKeyboardMarkup, InlineKeyboardButton
 from aiogram.filters import Command
+from aiogram.fsm.state import StatesGroup, State
+from aiogram.fsm.context import FSMContext
 
 db = TinyDB("db.json")
 users_table = db.table('users')
@@ -12,6 +14,12 @@ admins_table = db.table('admins')
 bot = Bot(token=API_TOKEN)
 dp = Dispatcher()
 router = Router()
+
+class ProfileForm(StatesGroup):
+    name = State()
+
+async def save_profile(name, user_id):
+    users_table.insert({"name": name, "user_id": user_id})
 
 menu_btns = ReplyKeyboardMarkup(
     keyboard=[
@@ -22,16 +30,28 @@ menu_btns = ReplyKeyboardMarkup(
     one_time_keyboard=True
 )
 
+
 # --- Хэндлеры ---
 @router.message(Command("start"))
 async def start_command(message: Message):
     await message.answer("Заготовка бота", reply_markup=menu_btns)
+    
 @router.message(F.text == "Админ меню") 
 async def echo(message: Message):
     await message.answer("Админ меню")
+    
 @router.message(F.text == "Профиль") 
-async def echo(message: Message):
-    await message.answer("Профиль")
+async def echo(message: Message, state: FSMContext):
+    await message.answer("Задайте никнейм:")
+    await state.set_state(ProfileForm.name)
+
+@router.message(ProfileForm.name) 
+async def echo(message: Message, state: FSMContext):
+    await state.update_data(name=message.text)
+    user_data = await state.get_data()
+    await save_profile(user_data["name"], message.from_user.id)
+    await message.answer('Данные сохраненны!')
+
 @router.message(F.text == "Помощь") 
 async def echo(message: Message):
     await message.answer("Помощь")
