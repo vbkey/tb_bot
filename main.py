eb03730fa29d0f2ee4f1c67ecb2356ee56e5b703
vbkey <vbkey0@gmail.com>
2025-09-26 23:51:46 +0500

    Added profile edit

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import asyncio
 from config import *
 from tinydb import TinyDB, Query
 from aiogram import Bot, Dispatcher, Router, F
-from aiogram.types import Message, ReplyKeyboardMarkup, KeyboardButton, InlineKeyboardMarkup, InlineKeyboardButton
+from aiogram.types import Message, ReplyKeyboardMarkup, KeyboardButton, InlineKeyboardMarkup, InlineKeyboardButton, CallbackQuery
 from aiogram.filters import Command
 from aiogram.fsm.state import StatesGroup, State
 from aiogram.fsm.context import FSMContext
@@ -18,13 +18,20 @@ router = Router()
 class ProfileForm(StatesGroup):
     name = State()
 
+class ProfileEdit(StatesGroup):
+    name = State()
+
 async def get_profile(user_id):
     User = Query()
     profile = await asyncio.to_thread(users_table.search, User.user_id == user_id)
     return profile[0]
 
+async def add_profile(name, user_id):
+    await asyncio.to_thread(users_table.insert, {"name": name, "user_id": user_id})
+
 async def save_profile(name, user_id):
-    users_table.insert({"name": name, "user_id": user_id})
+    User = Query()
+    await asyncio.to_thread(users_table.update, {"name": name}, User.user_id == user_id)
 
 menu_btns = ReplyKeyboardMarkup(
     keyboard=[
@@ -35,6 +42,11 @@ menu_btns = ReplyKeyboardMarkup(
     one_time_keyboard=True
 )
 
+profile_btns = InlineKeyboardMarkup(
+    inline_keyboard=[
+        [InlineKeyboardButton(text="Изменить", callback_data="edit_profile")]
+    ]
+)
 
 # --- Хэндлеры ---
 @router.message(Command("start"))
@@ -52,14 +64,29 @@ async def echo(message: Message, state: FSMContext):
         await message.answer("Задайте никнейм:")
         await state.set_state(ProfileForm.name)
     else:
-        await message.answer(f"Никнейм: {profile["name"]}\nКонец!")
+        await message.answer(f"Никнейм: {profile["name"]}\nКонец!", reply_markup=profile_btns)
 
-@router.message(ProfileForm.name) 
+@router.callback_query(F.data == "edit_profile")
+async def echo(callback: CallbackQuery, state: FSMContext):
+    await callback.message.answer("Задайте никнейм:")
+    await state.set_state(ProfileEdit.name)
+    await callback.answer()
+
+@router.message(ProfileEdit.name) 
 async def echo(message: Message, state: FSMContext):
     await state.update_data(name=message.text)
     user_data = await state.get_data()
     await save_profile(user_data["name"], message.from_user.id)
     await message.answer('Данные сохраненны!')
+    await state.clear()
+
+@router.message(ProfileForm.name) 
+async def echo(message: Message, state: FSMContext):
+    await state.update_data(name=message.text)
+    user_data = await state.get_data()
+    await add_profile(user_data["name"], message.from_user.id)
+    await message.answer('Данные сохраненны!')
+    await state.clear()
 
 @router.message(F.text == "Помощь") 
 async def echo(message: Message):
